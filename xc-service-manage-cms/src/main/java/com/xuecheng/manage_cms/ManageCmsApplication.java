2f7cee0e59299f1b407881aaf59bc2fad42595e9
Doran7632 <doran7632@163.com>
2020-07-22 18:29:28 +0800

    cms服务 ribbon和feign的配置

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 
-//@EnableDiscoveryClient //表示能发现注册中心中的服务
+@EnableDiscoveryClient //表示能发现注册中心中的服务
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
@@ -22,6 +23,7 @@ public class ManageCmsApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Administrator
  * @version 1.0
  **/
+@EnableFeignClients //开启feign调用
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.course")//扫描实体类
